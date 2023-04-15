f1bfd9a6df913186591fa2cbeec0227d824e4b05
Carlos Carvajal <cxcarvaj@espol.edu.ec>
2023-04-15 13:55:07 -0500

    Implementing the Floatting Button component

--- src/journal/components/index.js
+++ src/journal/components/index.js
@@ -1,3 +1,4 @@
+export * from './FloatingButton';
 export * from './ImageGallery';
 export * from './NavBar';
 export * from './SideBar';
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,15 @@
 import { Typography } from '@mui/material';
+
 import { JournalLayout } from '../layout/JournalLayout';
 import { NoteView, NothingSelectedView } from '../views';
+import { FloatingButton } from '../components';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* <NothingSelectedView /> */}
       <NoteView />
+      <FloatingButton />
     </JournalLayout>
   );
 };
