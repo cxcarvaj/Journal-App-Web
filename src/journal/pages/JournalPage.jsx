539cd97e41e774bf46a9ca3c29ea05d1bbc73c68
Carlos Carvajal <cxcarvaj@espol.edu.ec>
2023-04-15 13:22:13 -0500

    Implementing the nothing selected view

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,16 +1,11 @@
 import { Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
+import { NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio
-        adipisci aperiam, voluptatibus ipsa veniam aut. Dolores sint libero
-        cupiditate tempore accusamus veniam nisi earum. Molestias a nobis
-        voluptates nam ipsam!
-      </Typography>
-      {/* NothingSelected */}
+      <NothingSelectedView />
       {/* NoteView */}
     </JournalLayout>
   );
